0020bd55e3fdc4b32c76cfdfa77d1279af43efe9
marb61a <marb61a@gmail.com>
2018-06-04 12:10:20 -0400

    added chain validation to blockchain class and updated component template

--- src/classes/blockchain.class.ts
+++ src/classes/blockchain.class.ts
@@ -23,4 +23,24 @@ export class Blockchain {
     newBlock.hash = newBlock.calculateHash();
     this.chain.push(newBlock);
   }
+  
+  isChainValid(){
+    for(let i = 1; i < this.chain.length; i++ ){
+      const currentBlock = this.chain[i];
+      const previousBlock = this.chain[i - 1];
+      
+      // Validate data integrity
+      if(currentBlock.hash !== currentBlock.calculateHash()){
+        return false;
+      }
+      
+      // Validate the hash chain link
+      if(currentBlock.previousHash !== previousBlock.hash){
+         return false;
+      }
+      
+      // If there are no bad links or manipulated data
+      return true;
+    }
+  }
 }
