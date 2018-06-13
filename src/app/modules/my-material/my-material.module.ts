76a2e6027326762031c7905a0644d6b63ac8889b
marb61a <marb61a@gmail.com>
2018-06-13 16:41:53 -0400

    updated component html and css

--- src/app/modules/my-material/my-material.module.ts
+++ src/app/modules/my-material/my-material.module.ts
@@ -37,9 +37,39 @@ import { cdkTableModule } from '@angular/cdk/table';
 import 'hammerjs';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  exports: [
+    cdkTableModule,
+    MatAutoCompleteModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatStepperModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule
+  ]
 })
 export class MyMaterialModule { }
