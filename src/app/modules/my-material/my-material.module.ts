259bd78a3d6de8ee771c3adab294642622ca901f
marb61a <marb61a@gmail.com>
2018-06-13 16:29:33 -0400

    added imports to my-material module

--- src/app/modules/my-material/my-material.module.ts
+++ src/app/modules/my-material/my-material.module.ts
@@ -1,5 +1,40 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {
+  MatAutoCompleteModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatStepperModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule
+} from '@angular/material';
+
+import { cdkTableModule } from '@angular/cdk/table';
+import 'hammerjs';
 
 @NgModule({
   imports: [
